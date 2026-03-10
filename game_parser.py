94e91219cef4d8788bae7518732242a78ec1ae58
floatyboat <spencer@theboatfamily.com>
2026-03-10 15:01:22 -0600

    change format

--- game_parser.py
+++ game_parser.py
@@ -427,7 +427,7 @@ def format_scoreboard(results, reference_date, puzzle_numbers, title="Daily Game
                         j += 1
                     medal = f"{medals[rank - 1]} " if rank <= len(medals) else ""
                     players_str = " ".join(reversed(tied))
-                    message += f'{medal}{players_str}: {weighted} ({unweighted}/500)\n'
+                    message += f'{medal}{players_str}: {weighted} ({unweighted} unweighted)\n'
                     prev_val = score_tuple
                     i = j
                 message += '\n'
